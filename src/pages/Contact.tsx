52f3981a563ef0cf36e694cadaf706485146b1d6
Omar <oab102@hotmail.com>
2023-05-19 06:36:57 -0400

    add courses page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,11 +9,6 @@ const myStyle = {
 }
 
 const Contact = () => {
-	// useNav takes in a navLinkId and returns a ref
-	// this ref is used to register the navLinkId that's
-	// currently in view, and apply activeClass styling
-	// to the corresponding nav childElement
-
 	const contactRef = useNav('Contact');
 
 	return (
