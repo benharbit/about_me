745d2ff3252bf76e10dbb11c5696f70157f3f18a
Omar <oab102@hotmail.com>
2023-05-19 09:04:02 -0400

    updated Courses Data

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useNav } from '../customHooks/useNav';
 import './Page.css';
 import background2 from './unsplash22.jpg';
@@ -8,71 +8,47 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Link from '@mui/material/Link';
+import { coursesData } from './courseData'
+
 
 
 const Courses = () => {
-	const courseRef = useNav('courses');	
-	return (
-			<Box ref={courseRef}
-			id = 'courses'
-			sx={{ 
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				backgroundImage: `url(${background2})`,
-				backgroundColor: 'rgba(255, 255, 0, 0.2))',
-				height: '100vh',
-				width: '100vw',
+  const courseRef = useNav('courses');
+  return (
+    <Box ref={courseRef}
+      id='courses'
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundImage: `url(${background2})`,
+        backgroundColor: 'rgba(255, 255, 0, 0.2))',
+        height: '100vh',
+        width: '100vw',
 
-			}}>		
-				<Typography variant="h3" sx={{mt:10}} >Courses/Books/Tutorials</Typography>
-         
-            <Typography  variant="h5">
-                <List sx={{ width: '80%', maxWidth: 560, bgcolor: 'background.paper', borderRadius: '12px' }}>
-                  <ListItem alignItems="flex-start">
-                    <ListItemText
-                      primary="Operating Systems"
-                      secondary={
-                          <Link href="https://www.youtube.com/watch?v=vBURTt97EkA&list=PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O">Nesso Academy</Link>
-                      }
-                      />
-                  </ListItem>
-                  <ListItem alignItems="flex-start">
-                    <ListItemText> 
-                    <ListItemText
-                      primary="Design Patterns"
-                      secondary={
-                          <>Head First Pattern Design by Freeman, Bates, Sierra, Robson</>
-                      }
-                      />
-                    </ListItemText>
-                  </ListItem>
-                  <ListItem alignItems="flex-start">
-                    <ListItemText> 
-                    <ListItemText
-                      primary="Machine Learning"
-                      secondary={
-                        <Link href="http://cs231n.stanford.edu/">Stanford Machine Learning Vision cs231n</Link>
-                      }
-                      />
-                    </ListItemText>
-                  </ListItem>
-                  <ListItem alignItems="flex-start">
-                    <ListItemText> 
-                    <ListItemText
-                      primary="Data Structure and Algorithm "
-                      secondary={
-                        <>Grokking Algorithm by Aditya Bhargav</>
-                    }
-                      />
-                    </ListItemText>
-                  </ListItem>
-                </List>
-            </Typography>
-          
-				</Box>
-			
-	);
+      }}>
+      <Typography variant="h3" sx={{ mt: 10 }} >Courses/Books/Tutorials</Typography>
+
+      <Typography variant="h5">
+        <List sx={{ width: '80%', maxWidth: 560, bgcolor: 'background.paper', borderRadius: '12px' }}>
+          {coursesData.map((course) => {
+            return (
+              <ListItem alignItems="flex-start" >
+                <ListItemText sx={{ p:0, m:0}}
+                  primary={course.name}
+                  secondary={
+                    <Link href={course.link}>{course.description}</Link>
+                  }
+                />
+              </ListItem>
+            )
+          })}
+        </List>
+      </Typography>
+
+    </Box>
+
+  );
 };
 
 export default Courses;
---
+++ src/pages/courseData.ts
@@ -0,0 +1,32 @@
+export const coursesData = [
+  {
+    name: "Operating Systems",
+    link: "https://www.youtube.com/watch?v=vBURTt97EkA&list=PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O",
+    description: "Nesso Academy",
+  },
+  {
+    name: "Data Structure and Algorithm",
+    link: "https://www.amazon.com/Grokking-Algorithms-illustrated-programmers-curious/dp/1617292230/ref=sr_1_1?hvadid=570436991184&hvdev=c&hvlocphy=9010488&hvnetw=g&hvqmt=e&hvrand=8139555037474302019&hvtargid=kwd-341745437262&hydadcr=27888_14512639&keywords=grokking+algorithms+book&qid=1684499536&sr=8-1",
+    description: "Grokking Algorithm by Aditya Bhargav"
+  },
+  {
+    name: "Design Patterns",
+    description: "Head First Pattern Design by Freeman, Bates, Sierra, Robson",
+    link: "https://www.amazon.com/Head-First-Design-Patterns-Brain-Friendly/dp/0596007124/ref=sr_1_1?crid=66LXSR8LKRTX&keywords=Head+First+Pattern+Design+by+Freeman%2C+Bates%2C+Sierra%2C+Robson&qid=1684499753&sprefix=head+first+pattern+design+by+freeman%2C+bates%2C+sierra%2C+robson%2Caps%2C134&sr=8-1",
+  },
+  {
+    name: "Machine Learning",
+    description: "Stanford Machine Learning Vision cs231n",
+    link: "http://cs231n.stanford.edu/",
+  },
+  {
+    name: "Rust",
+    description: "Rust Programming Language by Steve Klabnik and Carol Nichols",
+    link: "https://www.amazon.com/Rust-Programming-Language-Steve-Klabnik/dp/1593278284",
+  },
+  {
+    name: "C++",
+    description: "C++ Course at UC Berkeley Master of Finaicial Program",
+    link: "https://mfe.haas.berkeley.edu/",
+  }
+]
